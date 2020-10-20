24cb9bd695117953906ec4e5b02765cddf7c153b
Albin Ljungdahl <al224iz@student.lnu.se>
2020-10-20 14:57:11 +0200

    Connected the chat application to the server

---
+++ src/chat.js
@@ -0,0 +1,29 @@
+let chatButton = document.getElementById('chatButton')
+let chatMessage = document.getElementById('chatMessage')
+
+console.log("Connecting to: " + "ws://vhost3.lnu.se:20080/socket/");
+let websocket = new WebSocket("ws://vhost3.lnu.se:20080/socket/");
+
+websocket.onopen = function() {
+    console.log("The websocket is now open.");
+    console.log(websocket);
+};
+
+websocket.onmessage = function(event) {
+    let sender = JSON.parse(event.data).username
+    let message = JSON.parse(event.data).data
+
+    console.log("Receiving message from: " + sender);
+    console.log("The message: " + message);
+};
+
+chatButton.addEventListener('click', event => {
+    let data = {
+        type: "message",
+        data: chatMessage.value,
+        username: "CrazyBananers",
+        key: "eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd"
+    }
+    websocket.send(JSON.stringify(data));
+    console.log("Sending message: " + chatMessage.value);
+})
