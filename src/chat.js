eeced92e8542bef3c9a40369222b7e6f33d6fbed
Albin Ljungdahl <al224iz@student.lnu.se>
2020-10-21 11:41:12 +0200

    Managed to open several chat windows

--- src/chat.js
+++ src/chat.js
@@ -1,9 +1,9 @@
-let chatButton = document.getElementById('chatButton')
-let chatMessage = document.getElementById('chatMessage')
-let chatMessages = document.getElementById('chatMessages')
+var chatButton = document.getElementById('chatButton')
+var chatMessage = document.getElementById('chatMessage')
+var chatMessages = document.getElementById('chatMessages')
 
 console.log("Connecting to: " + "ws://vhost3.lnu.se:20080/socket/");
-let websocket = new WebSocket("ws://vhost3.lnu.se:20080/socket/");
+var websocket = new WebSocket("ws://vhost3.lnu.se:20080/socket/");
 
 websocket.onopen = function() {
     console.log("The websocket is now open.");
@@ -11,20 +11,20 @@ websocket.onopen = function() {
 };
 
 websocket.onmessage = function(event) {
-    let sender = JSON.parse(event.data).username
-    let message = JSON.parse(event.data).data
+    var sender = JSON.parse(event.data).username
+    var message = JSON.parse(event.data).data
 
     console.log("Receiving message from: " + sender);
     console.log("The message: " + message);
     if(sender != "The Server") {
-        let paragraf = document.createElement("p")
+        var paragraf = document.createElement("p")
         paragraf.appendChild(document.createTextNode(sender + ": " + message))
         chatMessages.appendChild(paragraf)
     }
 };
 
 chatButton.addEventListener('click', event => {
-    let data = {
+    var data = {
         type: "message",
         data: chatMessage.value,
         username: "CrazyBananers",
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-var test = "hej";
+
+
+var generateChat = document.getElementById("generateChat")
+
+generateChat.addEventListener('click', event => {
+    var chatWindow = document.createElement("div")
+    chatWindow.className = "window"
+    chatWindow.id = "chat"
+
+    var chatMessage = document.createElement("input")
+    chatMessage.id = "chatMessage"
+    chatMessage.type = "text"
+    chatMessage.placeholder = "Write your message"
+    chatWindow.appendChild(chatMessage)
+
+    var chatButton = document.createElement("input")
+    chatButton.id = "chatButton"
+    chatButton.type = "button"
+    chatButton.value = "Send"
+    chatWindow.appendChild(chatButton)
+    
+    var chatMessages = document.createElement("div")
+    chatMessages.id = "chatMessages"
+    chatWindow.appendChild(chatMessages)
+
+    var script = document.createElement("script")
+    script.type = 'module'
+    script.src = "/_dist_/chat.js"
+    chatWindow.appendChild(script)
+
+    document.body.appendChild(chatWindow)
+})
